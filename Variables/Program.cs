04a865716a72bc038664496837695b2ec9db4c53
ucadena07 <ucadena07@gmail.com>
2023-03-25 09:37:23 -0500

    project setup

---
+++ Variables/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int int1 = 5_000_000; // -2,147,483,648 to 2,147,483,648 (4 bytes of memory)
+uint uint1 = 3_147_483_647; // 0 to 4,294,967,295 (4 bytes of memory)
+
+byte byte1 = 130; // 0 - 255 ( 1 byte of memory)
+sbyte sbyte2 = -100; // -128 to 127  ( 1 byte of memory)
+
+short short1 = 25_000; // -32768 to 32767 (2 bytes of memory)
+ushort ushort1 = 60_000; // 0 to 65,535 (2 bytes of memory)
+
+long long1 = 300_147_483_647; // (8 bytes of memory)
+ulong ulong1 = 545_300_147_483_647; // (8 bytes of memory)
