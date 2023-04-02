8baa0adb4ace1e65edf87e64c4745c599a4f47be
ucadena07 <ucadena07@gmail.com>
2023-04-02 07:03:03 -0500

    passing by ref

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -24,7 +24,7 @@ double Average(int[] numbers)
     return sum /numbers.Length;
 }
 
-Console.WriteLine(Average(new int[] { 1,5,7,2}));
+//Console.WriteLine(Average(new int[] { 1,5,7,2}));
 
 double Average2(params int[] numbers)
 {
@@ -36,4 +36,45 @@ double Average2(params int[] numbers)
     return sum / numbers.Length;
 }
 
-Console.WriteLine(Average2(1, 5, 7, 2));
+
+//Console.WriteLine(Average2(1, 5, 7, 2));
+
+
+//passing a value type
+int num1 = 5;
+
+void Double(ref int n)
+{
+    n *= 2;
+    Console.WriteLine($"the value inside is : {n}");
+}
+
+
+//Console.WriteLine($"the value outside is : {num1}");
+//Double(ref num1);
+//Console.WriteLine($"the value outside is : {num1}");
+
+//passing a ref type
+
+void DoubleArray(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] *= 2;
+    }
+}
+
+void PrintArray(int[] numbers)
+{
+    foreach (var item in numbers)
+    {
+        Console.WriteLine($"{item}");
+    }
+}
+
+var oArray = new int[] { 5, 10, 20 };
+
+PrintArray(oArray);
+DoubleArray(oArray);
+Console.WriteLine("===============");
+PrintArray(oArray);
