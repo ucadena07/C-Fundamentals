55af01f940f8509eb790f52af43c237dd333910e
ucadena07 <ucadena07@gmail.com>
2023-04-02 06:46:04 -0500

    passing params

---
+++ Functions/Program.cs
@@ -0,0 +1,39 @@
+﻿//Simple fuction
+string GetName()
+{
+    return "Ulises";
+}
+
+//No return value
+void PrintCurrentTime()
+{
+   // Console.WriteLine(DateTime.Now);
+}
+
+GetName();
+PrintCurrentTime();
+
+//passing data
+double Average(int[] numbers)
+{
+    var sum = 0.0;
+    foreach (var number in numbers)
+    {
+        sum += number;
+    }
+    return sum /numbers.Length;
+}
+
+Console.WriteLine(Average(new int[] { 1,5,7,2}));
+
+double Average2(params int[] numbers)
+{
+    var sum = 0.0;
+    foreach (var number in numbers)
+    {
+        sum += number;
+    }
+    return sum / numbers.Length;
+}
+
+Console.WriteLine(Average2(1, 5, 7, 2));
