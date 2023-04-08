ba685115f177a334f5a3b8a35fe00c1bf226d124
ucadena07 <ucadena07@gmail.com>
2023-04-08 08:20:06 -0500

    creating interfaces

---
+++ OOP/IMyInterface.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP;
+
+internal interface IMyInterface
+{
+    void Method1();
+    
+    string Method2(int param1);
+    bool MyProperty { get; set; }
+    int MyReadOnlyProp { get; }
+}
+
+internal interface IMySecondInterface
+{
+    void MethodFromSecondInterface();
+}
+
+public class MyClassWithAnInterface : IMyInterface, IMySecondInterface
+{
+    public bool MyProperty { get ; set; }
+
+    public int MyReadOnlyProp => 42;
+
+    public void Method1()
+    {
+        Console.WriteLine("This is method 1");
+    }
+
+    public string Method2(int param1)
+    {
+        return $"the param passed was {param1}";
+    }
+
+    public void MethodFromSecondInterface()
+    {
+        Console.WriteLine("Method from second interface");
+    }
+}
+internal struct MyStruct : IMySecondInterface
+{
+    public void MethodFromSecondInterface()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+internal record MyRecord(string param1) : IMySecondInterface
+{
+    public void MethodFromSecondInterface()
+    {
+        throw new NotImplementedException();
+    }
+}
