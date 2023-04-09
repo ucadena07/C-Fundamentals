49e61a22d02ef0f5c99bb6912ce6b8040b82beef
ucadena07 <ucadena07@gmail.com>
2023-04-09 08:16:25 -0500

    debugging

---
+++ DebuggingAndErrorHandling/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+var myWritter = new TextWriterTraceListener(Console.Out);
+Trace.Listeners.Add(myWritter);
+
+Debug.WriteLine("This message will only appear in debug mode");
+
+Debug.WriteLine("==================");
+var test = 1;
+test++;
+
+Add(5,6);
+
+int Add(int a, int b)
+{
+    int c = a + b;
+    return c;
+}
+
+void MethodA()
+{
+
+}
+
+void MethodB()
+{
+
+}
+
+
+MethodB();
+MethodA();
+MethodB();
+
+
+void Process(int a, string name)
+{
+    var tupple = (a, name);
+    a = 3;
+    int b = a + 4;
+    b -= 4;
+    int[] numbers = {1,2,3,4,5,6,7};
+}
+
+Process(7, "Ulises");
+
+ClassA.ProcessA();
+
+internal class ClassA
+{
+    public static void ProcessA()
+    {
+        ClassB.ProcessB();
+    }
+}
+
+internal class ClassB
+{
+    public static void ProcessB()
+    {
+        ClassC.ProcessC();
+    }
+
+}
+
+internal class ClassC
+{
+    public static void ProcessC()
+    {
+        throw new Exception();
+    }
+}
+
