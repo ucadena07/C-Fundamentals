0f20fac5f770a4685b3473beec7251c6b9c904d5
Ulises Cadena <ucadena07@gmail.com>
2023-11-02 06:09:30 -0500

    introduction to generics

---
+++ GenericsAndCollections/ArrayUtils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAndCollections;
+
+internal class ArrayUtils<T> : IArrayUtilities<T>	
+{
+	public int ComputeLengthOfArray(T[] array)
+	{
+		return array.Length;
+	}
+
+	public T FirstElementOrDefault(T[] array)
+	{
+		if (array.Length == 0) return default;
+		return array[0];
+	}
+}
+
+public interface IArrayUtilities<T>
+{
+	int ComputeLengthOfArray(T[] array);
+	T FirstElementOrDefault(T[] array);
+}
---
+++ GenericsAndCollections/Program.cs
@@ -0,0 +1,22 @@
+﻿using GenericsAndCollections;
+
+int[] numbers = {1,2,3};
+string[] names = { "uli", "haylee", "rico", "nigel" };
+DateTime[] dates = {DateTime.Today, DateTime.Today.AddDays(1)};
+
+//numbers = new int[0];
+//names = new string[0];
+//dates = new DateTime[0];
+
+var utilsInt = new ArrayUtils<int>();
+var utilsString = new ArrayUtils<string>();
+var utilsDate = new ArrayUtils<DateTime>();
+
+Console.WriteLine(utilsInt.ComputeLengthOfArray(numbers));
+Console.WriteLine(utilsString.ComputeLengthOfArray(names));
+Console.WriteLine(utilsDate.ComputeLengthOfArray(dates));
+Console.WriteLine("================================");
+Console.WriteLine(utilsInt.FirstElementOrDefault(numbers));	
+Console.WriteLine(utilsString.FirstElementOrDefault(names));	
+Console.WriteLine(utilsDate.FirstElementOrDefault(dates));	
+
