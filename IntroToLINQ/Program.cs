5578b855ea37aa2ba697a15d401d5aea94a2effa
Ulises Cadena <ucadena07@gmail.com>
2023-11-04 07:26:09 -0500

    LINQ method vs query syntax 2

--- IntroToLINQ/Program.cs
+++ IntroToLINQ/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] numbers = {1,2,3,4,5,6,7};    
+
+//This is LINQ (Method syntax)
+var evenNumbers = numbers.Where(x => x % 2 == 0).ToList();  
+
+
+//Form 2: Query syntax
+var evenNumbers2 = (from n in numbers
+                    where n % 2 == 0
+                    select n).ToList();
+
+
