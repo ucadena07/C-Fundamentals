83a253447aca98a0df282aefb8ecd8cf421d5fc3
Ulises Cadena <ucadena07@gmail.com>
2023-11-05 07:54:54 -0600

    directory class

--- WorkingWithFiles/Program.cs
+++ WorkingWithFiles/Program.cs
@@ -4,9 +4,26 @@ if (File.Exists(fileLocation))
     var lines = File.ReadAllLines(@"C:\Users\ucade_lbz6a\OneDrive\Documents\ASP.NET\Fundamentals\Fundamentals\WorkingWithFiles\exampleLines.txt");
     var copyLocation = @"C:\Users\ucade_lbz6a\OneDrive\Documents\ASP.NET\Fundamentals\Fundamentals\WorkingWithFiles\exampleLines_copy.txt";
     File.Copy(fileLocation, copyLocation,overwrite: true);
-    Console.WriteLine(lines);
+    //Console.WriteLine(lines);
 }
 
+var route = @"C:\Users\ucade_lbz6a\OneDrive\Documents\ASP.NET\Fundamentals\Fundamentals\WorkingWithFiles\MyDir\sub1\sub2";
+var sub2 = @"C:\Users\ucade_lbz6a\OneDrive\Documents\ASP.NET\Fundamentals\Fundamentals\WorkingWithFiles\MyDir\sub1\sub2";
+Directory.CreateDirectory(route);
+
+var routeToEnumerate = @"C:\Users\ucade_lbz6a\OneDrive\Documents\ASP.NET\Fundamentals\Fundamentals\WorkingWithFiles";
+var routes = Directory.EnumerateDirectories(routeToEnumerate,"*",SearchOption.AllDirectories);
+foreach (var routePath in routes)
+{
+    //Console.WriteLine(routePath);
+}
+
+var files = Directory.EnumerateFiles(routeToEnumerate, "*.json", SearchOption.AllDirectories);
+foreach (var file in files)
+{
+    Console.WriteLine(file);
+}
+Directory.Delete(sub2);
 
 //var content = File.ReadAllText(@"C:\Users\ucade_lbz6a\OneDrive\Documents\ASP.NET\Fundamentals\Fundamentals\WorkingWithFiles\example.txt");
 
