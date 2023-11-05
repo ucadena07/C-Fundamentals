ccd3214b881843bb786b5d858be43f74b8c1b577
Ulises Cadena <ucadena07@gmail.com>
2023-11-05 07:44:56 -0600

    reading and copying files

---
+++ WorkingWithFiles/Program.cs
@@ -0,0 +1,12 @@
+﻿var fileLocation = @"C:\Users\ucade_lbz6a\OneDrive\Documents\ASP.NET\Fundamentals\Fundamentals\WorkingWithFiles\exampleLines.txt";
+if (File.Exists(fileLocation))
+{
+    var lines = File.ReadAllLines(@"C:\Users\ucade_lbz6a\OneDrive\Documents\ASP.NET\Fundamentals\Fundamentals\WorkingWithFiles\exampleLines.txt");
+    var copyLocation = @"C:\Users\ucade_lbz6a\OneDrive\Documents\ASP.NET\Fundamentals\Fundamentals\WorkingWithFiles\exampleLines_copy.txt";
+    File.Copy(fileLocation, copyLocation,overwrite: true);
+    Console.WriteLine(lines);
+}
+
+
+//var content = File.ReadAllText(@"C:\Users\ucade_lbz6a\OneDrive\Documents\ASP.NET\Fundamentals\Fundamentals\WorkingWithFiles\example.txt");
+
