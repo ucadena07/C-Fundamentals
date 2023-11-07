ec9a4b5906fa8304d3ae4077350158aa8aa3792d
Ulises Cadena <ucadena07@gmail.com>
2023-11-07 06:37:22 -0600

    reading data

--- AdoNet/Program.cs
+++ AdoNet/Program.cs
@@ -2,41 +2,35 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
- 
+using System.Data;
+
 using IHost host = Host.CreateDefaultBuilder(args).Build();
 var configuration = host.Services.GetService<IConfiguration>();
 var connectionString = configuration.GetConnectionString("DefaultConnection");
 
-var name = "Alondra Cadena";
+
 try
 {
 
     using (var connection = new SqlConnection(connectionString))
 	{
 		connection.Open();
-        var query = @"People_Insert";
+        var query = @"SELECT * FROM People";
         using (var command = new SqlCommand(query, connection))
         {
-            command.CommandType = System.Data.CommandType.StoredProcedure;
-            var paramterId = new SqlParameter
+            using(SqlDataAdapter adapter = new SqlDataAdapter(command))
             {
-                ParameterName = "@id",
-                Direction= System.Data.ParameterDirection.Output,
-                DbType = System.Data.DbType.Int32
-            };
-            command.Parameters.Add(paramterId);
-
-            command.Parameters.Add(new SqlParameter("@name", name));
-            var rowsAffected = await command.ExecuteNonQueryAsync();
-            Console.WriteLine($"Affected rows {rowsAffected}");
-            var id = (int)paramterId.Value;
-            Console.WriteLine($"new id: {id}");
+                var dt = new DataTable();
+                adapter.Fill(dt);
+                foreach (DataRow dataRow in dt.Rows)
+                {
+                    Console.WriteLine($"{dataRow["Id"]} | {dataRow["Name"]}");
+                }
+            }
         }
     }
 }
 catch (Exception ex)
 {
-
-    Console.WriteLine("could not open the connection");
     Console.WriteLine(ex.Message);
 }
