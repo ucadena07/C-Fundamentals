a984cb59854366929cbd4f72ae42ecc9acd15d4f
Ulises Cadena <ucadena07@gmail.com>
2023-11-07 07:00:25 -0600

    instantiating methods with reflection

--- ReflectionAndMetadata/Program.cs
+++ ReflectionAndMetadata/Program.cs
@@ -1,9 +1,21 @@
-﻿Type t = typeof(int);
+﻿//Type t = typeof(int);
 
-int age = 999;
-Type dataTypeOfAge = age.GetType();
+//int age = 999;
+//Type dataTypeOfAge = age.GetType();
 
-Type dataTypeOfInt = Type.GetType("System.Int32");
+//Type dataTypeOfInt = Type.GetType("System.Int32");
 
-Console.WriteLine($"Is {t.Name} an array? {t.IsArray}");
-Console.WriteLine($"The max value of an integer is {t.GetField("MaxValue").GetValue(default(int))}");
+//Console.WriteLine($"Is {t.Name} an array? {t.IsArray}");
+//Console.WriteLine($"The max value of an integer is {t.GetField("MaxValue").GetValue(default(int))}");
+
+//Example 1: Instantiating a class by the type
+using ReflectionAndMetadata;
+
+var personWithoutNameViaType = (Person)Activator.CreateInstance(typeof(Person));
+
+
+//Example 1: Instantiating a class by the name and assembly
+var fullNamePerson = typeof(Person).FullName;   
+var assemblyOfPersonClass = typeof(Person).Assembly.GetName().Name;
+var personWithoutNameViaString = (Person)Activator.CreateInstance(assemblyOfPersonClass, fullNamePerson).Unwrap();
+Console.WriteLine($"Person instantiated by its name {personWithoutNameViaString}");
