854c0f36b4a5461cd8c16d61dd70b3139b43e0ff
Ulises Cadena <ucadena07@gmail.com>
2023-11-07 06:50:32 -0600

    Propterties of Type

---
+++ ReflectionAndMetadata/Program.cs
@@ -0,0 +1,9 @@
+﻿Type t = typeof(int);
+
+int age = 999;
+Type dataTypeOfAge = age.GetType();
+
+Type dataTypeOfInt = Type.GetType("System.Int32");
+
+Console.WriteLine($"Is {t.Name} an array? {t.IsArray}");
+Console.WriteLine($"The max value of an integer is {t.GetField("MaxValue").GetValue(default(int))}");
