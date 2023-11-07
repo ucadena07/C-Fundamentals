26084f78ea59d8095aea8cc6bf134041d1c113a3
Ulises Cadena <ucadena07@gmail.com>
2023-11-07 06:23:21 -0600

    stored procedure and output parameter

--- AdoNet/Program.cs
+++ AdoNet/Program.cs
@@ -7,20 +7,30 @@ using IHost host = Host.CreateDefaultBuilder(args).Build();
 var configuration = host.Services.GetService<IConfiguration>();
 var connectionString = configuration.GetConnectionString("DefaultConnection");
 
-var name = "Nigel Cadena";
+var name = "Alondra Cadena";
 try
 {
 
     using (var connection = new SqlConnection(connectionString))
 	{
 		connection.Open();
-        var query = @"INSERT INTO People (Name)
-                        VALUES(@name)";
+        var query = @"People_Insert";
         using (var command = new SqlCommand(query, connection))
         {
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            var paramterId = new SqlParameter
+            {
+                ParameterName = "@id",
+                Direction= System.Data.ParameterDirection.Output,
+                DbType = System.Data.DbType.Int32
+            };
+            command.Parameters.Add(paramterId);
+
             command.Parameters.Add(new SqlParameter("@name", name));
             var rowsAffected = await command.ExecuteNonQueryAsync();
             Console.WriteLine($"Affected rows {rowsAffected}");
+            var id = (int)paramterId.Value;
+            Console.WriteLine($"new id: {id}");
         }
     }
 }
